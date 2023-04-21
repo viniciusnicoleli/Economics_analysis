9cf9031551936da63f5d3a4446c1e816fabe3028
Vinicius Nicoleli <viniciusnicoleli@gmail.com>
2023-04-20 22:00:05 -0300

    third

--- main.R
+++ main.R
@@ -7,7 +7,7 @@ setwd("C:\\Users\\vinic\\Documents\\Projetos\\emilly_work")
 
 # Coletando os dados:
 
-df2 <- read.csv(file = "testing.csv",sep = ';')
+df2 <- read.csv(file = "microdados_enade_2018econ.csv",fileEncoding = 'UTF-16LE',sep = '\t')
 colnames(df2)
 dim(df2)
 
@@ -35,7 +35,7 @@ dim(df_feat)
 df_feat <- df_feat %>% rename('Codigo_curso' = 'Co.Curso', 
                               'Nota_geral' = 'Nt.Ger',
                               'Nota_form_geral' = 'Nt.Fg',
-                              'EAD?' = 'Co.Modalidade',
+                              'EAD' = 'Co.Modalidade',
                               'Curso_grupo' = 'Co.Grupo',
                               'Estado_curso' = 'Co.Uf.Curso',
                               'Idade' = 'Nu.Idade',
@@ -74,6 +74,7 @@ df_feat$Nota_form_geral <- as.numeric(df_feat$Nota_form_geral)
 
 df_feat$Estado_civil <- as.factor(df_feat$Estado_civil)
 df_feat$Raça <- as.factor(df_feat$Raça)
+df_feat$Sexo <- as.factor(df_feat$Sexo)
 df_feat$Nacionalidade <- as.factor(df_feat$Nacionalidade)
 df_feat$Esforco_aluno <- as.factor(df_feat$Esforco_aluno)
 df_feat$Tipo_ensino_medio <- as.factor(df_feat$Tipo_ensino_medio)
@@ -85,6 +86,8 @@ colnames(df_feat)
 
 # --------------------
 
+hist(df_feat$Nota_geral)
+
 # Rodando primeiro modelo:
 
 df_model <- df_feat %>% select(-c(Curso_grupo,Codigo_curso,Nota_form_geral,
@@ -92,17 +95,60 @@ df_model <- df_feat %>% select(-c(Curso_grupo,Codigo_curso,Nota_form_geral,
                                   Tipo_presenca_geral,Tipo_ensino_medio,
                                   Nacionalidade))
 
-modelm <- lm(df_model$'Nota_geral' ~ ., data = df_model,na.action = na.omit)
+str(df_model)
+
+
+library(ggcorrplot)
+model.matrix(~0+., data=df_model) %>% 
+  cor(use="pairwise.complete.obs") %>% 
+  ggcorrplot(show.diag=FALSE, type="lower", lab=TRUE, lab_size=2)
+
+
+library(lme4)
+
+colnames(df_model)
+hist(df_model$Nota_geral)
+modelm <- glmer(df_model$'Nota_geral' ~ Estado_civil + Turno_graduacao 
+             + Raça + Estado_civil + Programa_governo_facul + EAD + Idade 
+             + Politicas_inclusao_facul
+             + Esforco_aluno,data = df_model,
+             family = 'beta')
+
+modelm2 <- glm(formula = Nota_geral ~ Estado_civil + Turno_graduacao 
+                 + Raça + Estado_civil + Programa_governo_facul + EAD + Idade 
+                 + Politicas_inclusao_facul
+                 + Esforco_aluno,data = df_model,family = gaussian)
+
+summary(modelm2)
+
+help(glm)
+help(plm)
 summary(modelm)
-anova(modelm)
+# -----------------------------------------------------------------------------
+df_model <- tibble::rowid_to_column(df_model, "ID")
+head(df_model)
+
+
+
+
+modelm <- plm(df_model$'Nota_geral' ~ Estado_civil + Turno_graduacao +
+               Raça + Estado_civil + Programa_governo_facul + EAD + Idade + Politicas_inclusao_facul
+             + Esforco_aluno,data = df_model,model='within',index=Programa_governo_facul)
+
+modelm <- plm(df_model$'Nota_geral' ~ Estado_civil + Turno_graduacao +
+                Raça + Estado_civil + Programa_governo_facul + EAD + Idade + Politicas_inclusao_facul
+              + Esforco_aluno,data = df_model,model='random',index=)
+
+
+
 
 model0 <- plm(df_model$'Nota_geral' ~ ., data = df_model,na.action = na.omit,model = 'within')
 
 model1 <- plm(df_model$'Nota_geral' ~ ., data = df_model,na.action = na.omit,model = 'random')
 
-phtest(model0,model1)
+phtest(modelm,model1)
 
 summary(model0)
 anova(model0)
 
-help(lm)
+help(plm)
